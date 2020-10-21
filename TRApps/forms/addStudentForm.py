f3824fc2ed5f531ad17818f8761d61db8182cfb5
Iqbal Tawakal <this.iqbal@gmail.com>
2020-10-22 00:04:14 +0700

    add mhs

--- TRApps/forms/addStudentForm.py
+++ TRApps/forms/addStudentForm.py
@@ -1,7 +1,13 @@
 from django import forms
 from ..model.tabelMahasiswa import Mahasiswa
+from crispy_forms.helper import FormHelper
+
 
 class mahasiswaForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.disable_csrf = True
+
     class Meta:
         model = Mahasiswa
         fields = [
--- TRApps/settings.py
+++ TRApps/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TRApps'
+    'TRApps',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
